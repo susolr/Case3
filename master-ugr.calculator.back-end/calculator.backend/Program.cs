0e08679ef762b4698f9db6d415b0856572ec0fe5
Nico Afonso <nicolas.afonso@se.com>
2024-12-08 10:24:46 +0100

    Backend with references to calculator lib

--- master-ugr.calculator.back-end/calculator.backend/Program.cs
+++ master-ugr.calculator.back-end/calculator.backend/Program.cs
@@ -1,4 +1,6 @@
 
+using Swashbuckle.AspNetCore.Swagger;
+
 namespace calculator.backend
 {
     public class Program
@@ -12,19 +14,28 @@ namespace calculator.backend
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Calculator API",
+                    Description = "An API for performing basic calculator operations"
+                });
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calculator API v1");
+            });
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
