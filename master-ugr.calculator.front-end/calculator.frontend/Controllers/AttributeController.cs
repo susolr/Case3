4e27ecfeb1cfdeba6bb6fc99ce1b2c641510759a
Rafael Díaz <rafandpufff@gmail.com>
2025-01-13 22:47:46 +0100

    Refactor front-end controllers

--- master-ugr.calculator.front-end/calculator.frontend/Controllers/AttributeController.cs
+++ master-ugr.calculator.front-end/calculator.frontend/Controllers/AttributeController.cs
@@ -5,70 +5,64 @@ namespace calculator.frontend.Controllers
 {
     public class AttributeController : Controller
     {
+        private string base_url =
+            Environment.GetEnvironmentVariable("CALCULATOR_BACKEND_URL") ??
+            "https://susolr-calculator-backend-uat.azurewebsites.net";
+
+        const string api = "api/Calculator";
+
         public IActionResult Index()
         {
             return View();
         }
-        private string base_url =
-            Environment.GetEnvironmentVariable("CALCULATOR_BACKEND_URL") ??
-            "https://susolr-calculator-backend-uat.azurewebsites.net";
-        const string api = "api/Calculator";
-        private KeyValuePair<string,string> ExecuteOperation(string number)
-        {
-            bool? raw_prime =  null;
-            bool? raw_odd = null;
-            var clientHandler = new HttpClientHandler();
-            var client = new HttpClient(clientHandler);
-            var url = $"{base_url}/api/Calculator/number_attribute?number={number}";
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri(url),
-            };
-            using (var response = client.Send(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var body = response.Content.ReadAsStringAsync().Result;
-                var json = JObject.Parse(body);
-                var prime = json["prime"];
-                var odd = json["odd"];
-                if (prime != null)
-                {
-                    raw_prime = prime.Value<bool>();
-                }
-                if (odd != null)
-                {
-                    raw_odd = odd.Value<bool>();
-                }
 
-            }
-            var isPrime = "unknown";
-            if (raw_prime != null && raw_prime.Value)
-            {
-                isPrime = "Yes";
-            }
-            else if (raw_prime != null && !raw_prime.Value)
-            {
-                isPrime = "No";
-            }
-            var isOdd = "unknown";
-            if (raw_odd != null && raw_odd.Value)
-            {
-                isOdd = "Yes";
-            }
-            else if (raw_odd != null && !raw_odd.Value)
-            {
-                isOdd = "No";
-            }
-            return new KeyValuePair<string,string>(isPrime,isOdd);
-        }
         [HttpPost]
-        public ActionResult Index(string number)
+        public IActionResult Index(string number)
         {
             var result = ExecuteOperation(number);
+
             ViewBag.IsPrime = result.Key;
             ViewBag.IsOdd = result.Value;
+
             return View();
+        }
+
+        private KeyValuePair<string, string> ExecuteOperation(string number)
+        {
+            string url = BuildUrl(number);
+
+            var attributes = FetchAttributesFromApi(url);
+
+            return ParseAttributes(attributes);
+        }
+
+        private string BuildUrl(string number)
+        {
+            return $"{base_url}/api/Calculator/number_attribute?number={number}";
+        }
+
+        private JObject FetchAttributesFromApi(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(url).Result;
+
+                response.EnsureSuccessStatusCode();
+
+                var body = response.Content.ReadAsStringAsync().Result;
+                return JObject.Parse(body);
+            }
+        }
+
+        private KeyValuePair<string, string> ParseAttributes(JObject attributes)
+        {
+            bool? rawPrime = attributes["prime"]?.Value<bool>();
+            bool? rawOdd = attributes["odd"]?.Value<bool>();
+
+            string isPrime = rawPrime == true ? "Yes" : rawPrime == false ? "No" : "unknown";
+            string isOdd = rawOdd == true ? "Yes" : rawOdd == false ? "No" : "unknown";
+
+            return new KeyValuePair<string, string>(isPrime, isOdd);
         }
     }
 }
