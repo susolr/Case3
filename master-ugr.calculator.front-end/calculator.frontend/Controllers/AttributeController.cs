c8b3747aaa23f788c56f522e38abea2af6c161df
susolr <jlopez@JesusDesktop.>
2025-01-16 11:16:26 +0100

    Adding new feature square root

--- master-ugr.calculator.front-end/calculator.frontend/Controllers/AttributeController.cs
+++ master-ugr.calculator.front-end/calculator.frontend/Controllers/AttributeController.cs
@@ -3,6 +3,19 @@ using Newtonsoft.Json.Linq;
 
 namespace calculator.frontend.Controllers
 {
+    public struct Triple
+    {
+        public string isPrime { get; }
+        public string isOdd { get; }
+        public string sqrt { get; }
+
+        public Triple(string prime, string odd, string sqrt)
+        {
+            isPrime = prime;
+            isOdd = odd;
+            sqrt = sqrt;
+        }
+    }
     public class AttributeController : Controller
     {
         private string base_url =
@@ -16,14 +29,70 @@ namespace calculator.frontend.Controllers
             return View();
         }
 
+        private Triple ExecuteOperation(string number)
+        {
+            bool? raw_prime =  null;
+            bool? raw_odd = null;
+            double? raw_sqrt = null;
+            var clientHandler = new HttpClientHandler();
+            var client = new HttpClient(clientHandler);
+            var url = $"{base_url}/api/Calculator/number_attribute?number={number}";
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url),
+            };
+            using (var response = client.Send(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = response.Content.ReadAsStringAsync().Result;
+                var json = JObject.Parse(body);
+                var prime = json["prime"];
+                var odd = json["odd"];
+                var srqt = json["square_root"];
+                if (prime != null)
+                {
+                    raw_prime = prime.Value<bool>();
+                }
+                if (odd != null)
+                {
+                    raw_odd = odd.Value<bool>();
+                }
+                if(srqt != null){
+                    raw_sqrt = srqt.Value<double>();
+                }
+
+            }
+            var isPrime = "unknown";
+            if (raw_prime != null && raw_prime.Value)
+            {
+                isPrime = "Yes";
+            }
+            else if (raw_prime != null && !raw_prime.Value)
+            {
+                isPrime = "No";
+            }
+            var isOdd = "unknown";
+            if (raw_odd != null && raw_odd.Value)
+            {
+                isOdd = "Yes";
+            }
+            else if (raw_odd != null && !raw_odd.Value)
+            {
+                isOdd = "No";
+            }
+            var sqrtResult = "unknown";
+            if(raw_sqrt != null){
+                sqrtResult = raw_sqrt.Value.ToString("N0");
+            }
+            return new Triple(isPrime,isOdd,sqrtResult);
+        }
         [HttpPost]
         public IActionResult Index(string number)
         {
             var result = ExecuteOperation(number);
-
             ViewBag.IsPrime = result.Key;
             ViewBag.IsOdd = result.Value;
-
             return View();
         }
 
